caac1bc2fba421563ae327a5fc43a04f1718656e
nikhilsharma110994 <nikhilsharma110994@gmail.com>
2017-12-18 11:48:02 +0530

    set process's affinity

---
+++ setcpu.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Run this file with command taskset -c 1 setcpu.sh. Then enter top command and press 1, we will notice that 1st cpu core will be 100% occupied.  
+while true
+do
+ true
+done
