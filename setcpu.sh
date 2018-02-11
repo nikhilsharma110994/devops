a25489b725d51ae3ee55f8d7cb61e77e7242622a
Nikhil Sharma <34493393+nikhilsharma110994@users.noreply.github.com>
2018-02-11 21:49:01 +0530

    Update setcpu.sh

--- setcpu.sh
+++ setcpu.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 echo "Hello"
+
+for (( c=2; c<=7; c++ ))
+do 
+echo $c
+done
 # Run this file with command taskset -c 1 setcpu.sh. Then enter top command and press 1, we will notice that 1st cpu core will be 100% occupied.  
 #while true
 #do
